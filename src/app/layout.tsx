8ce436216971e78ad0c71ee74de93697c0a67f99
Tomas <talk.gtg@gmail.com>
2024-05-21 20:38:42 -0300

    Possible fixing deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import 'swiper/css';
 import 'swiper/css/effect-cards';
-import { AuthContextProvider } from "../context/AuthContext";
+import {AuthContextProvider} from "@/context/AuthContext";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
